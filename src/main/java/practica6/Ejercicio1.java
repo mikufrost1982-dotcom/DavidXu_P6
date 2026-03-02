792ae8e2d98ea1da701d57851e81ce47ee5a3ff7
mikufrost1982-dotcom <mikufrost1982@gmail.com>
2026-03-02 11:14:11 +0100

    Reestructurar el código como guardar la pista

--- src/main/java/practica6/Ejercicio1.java
+++ src/main/java/practica6/Ejercicio1.java
@@ -1,5 +1,59 @@
 package practica6;
 
-public class Ejercicio1 {
+import java.util.Scanner;
 
+public class Ejercicio1 {
+    public static Scanner sc=new Scanner(System.in);
+
+    public static void ahorcado(String jugadorA, char jugadorB){
+        String adivinar="";
+        char minusJugadorB=Character.toLowerCase(jugadorB);
+        String minusJugadorA=jugadorA.toLowerCase();
+
+        for (int i=0;i<jugadorA.length();i++){
+            if(minusJugadorA.charAt(i)==minusJugadorB){
+                adivinar+=jugadorA.charAt(i);
+            }else {
+                adivinar+="-";
+            }
+        }
+        System.out.print(adivinar);
+    }
+
+    //public static String guardaPista(String jugadorA, String pista){
+
+    //}
+
+    public static boolean ganador(String jugadorA, String pista){
+
+        return jugadorA.equalsIgnoreCase(pista);
+    }
+    static void main() {
+        String jugadorA, ahorcado, pista = "";
+        char jugadorB = ' ';
+        int contador=7;
+        boolean ganador;
+
+        System.out.println("\u001B[31m\u001B[1mJugador A.\u001B[0m Escriba una palabra: ");
+        jugadorA=sc.nextLine();
+
+        while(contador>0){
+            ahorcado(jugadorA,jugadorB);
+            System.out.println(" Intentos: "+contador);
+            System.out.print("\u001B[34m\u001B[1mJugador B.\u001B[0m Letra: ");
+            jugadorB=sc.next().charAt(0);
+
+
+            ganador=ganador(jugadorA,jugadorB);
+            if (!ganador){
+                System.out.println("Gana el jugador A");
+                contador--;
+            }
+            if (ganador){
+                System.out.println("Gana el jugador B");
+            }
+
+
+        }
+    }
 }
