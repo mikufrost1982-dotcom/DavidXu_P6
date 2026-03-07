51b52568088b0bf6c0ecc5843401ca2aac109ab7
mikufrost1982-dotcom <mikufrost1982@gmail.com>
2026-03-07 18:38:26 +0100

    hecho falta comentarlo

--- src/main/java/practica6/Ejercicio1.java
+++ src/main/java/practica6/Ejercicio1.java
@@ -1,59 +1,68 @@
 package practica6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio1 {
     public static Scanner sc=new Scanner(System.in);
 
-    public static void ahorcado(String jugadorA, char jugadorB){
-        String adivinar="";
-        char minusJugadorB=Character.toLowerCase(jugadorB);
-        String minusJugadorA=jugadorA.toLowerCase();
+    public static void inicioPista(char[]palabraSecreta,char[]pista){
+        for (int i = 0; i < palabraSecreta.length; i++) {
+            pista[i]='-';
+        }
+    }
 
-        for (int i=0;i<jugadorA.length();i++){
-            if(minusJugadorA.charAt(i)==minusJugadorB){
-                adivinar+=jugadorA.charAt(i);
-            }else {
-                adivinar+="-";
+    public static void actualizarPista(char letra,char[]palabraSecreta, char[]pistaActual){
+
+        for (int i = 0; i < palabraSecreta.length; i++) {
+            if (Character.toLowerCase(palabraSecreta[i])==Character.toLowerCase(letra)){
+                pistaActual[i]=palabraSecreta[i];
             }
         }
-        System.out.print(adivinar);
     }
 
-    //public static String guardaPista(String jugadorA, String pista){
+    public static boolean ganadorPartida (char[]pista, char[]palabraSecreta,char letra){
+        boolean gana = false;
 
-    //}
-
-    public static boolean ganador(String jugadorA, String pista){
-
-        return jugadorA.equalsIgnoreCase(pista);
+        for (char elemento : palabraSecreta) {
+            if (Character.toLowerCase(elemento) == Character.toLowerCase(letra)) {
+                gana = true;
+            }
+        }
+        return gana;
     }
     static void main() {
-        String jugadorA, ahorcado, pista = "";
-        char jugadorB = ' ';
+        String jugadorA;
+        char jugadorB;
         int contador=7;
-        boolean ganador;
+        boolean ganador = false;
+        char[]palabraSecreta, pista;
 
         System.out.println("\u001B[31m\u001B[1mJugador A.\u001B[0m Escriba una palabra: ");
         jugadorA=sc.nextLine();
 
-        while(contador>0){
-            ahorcado(jugadorA,jugadorB);
-            System.out.println(" Intentos: "+contador);
+        palabraSecreta=jugadorA.toCharArray();
+        pista=new char[palabraSecreta.length];
+        inicioPista(palabraSecreta,pista);
+
+        while(!Arrays.equals(pista,palabraSecreta)&&contador>0){
+            System.out.println(String.valueOf(pista) +" Intentos: "+contador);
             System.out.print("\u001B[34m\u001B[1mJugador B.\u001B[0m Letra: ");
             jugadorB=sc.next().charAt(0);
 
+            actualizarPista(jugadorB,palabraSecreta,pista);
 
-            ganador=ganador(jugadorA,jugadorB);
+            ganador=ganadorPartida(pista,palabraSecreta,jugadorB);
+
             if (!ganador){
-                System.out.println("Gana el jugador A");
                 contador--;
             }
-            if (ganador){
-                System.out.println("Gana el jugador B");
-            }
 
-
+        }
+        if (contador==0){
+            System.out.println("Gana el jugador A");
+        }else {
+            System.out.println("Gana el jugador B");
         }
     }
 }
