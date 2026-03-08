4acbee3be9a83901209e05ba1e88a081702d4795
mikufrost1982-dotcom <mikufrost1982@gmail.com>
2026-03-08 22:10:48 +0100

    hecho falta comentar

--- src/main/java/practica6/Ejercicio1.java
+++ src/main/java/practica6/Ejercicio1.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-    public static Scanner sc=new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
 
-    public static void inicioPista(char[]palabraSecreta,char[]pista){
+    public static void inicioPista(char[] palabraSecreta, char[] pista) {
         for (int i = 0; i < palabraSecreta.length; i++) {
-            pista[i]='-';
+            pista[i] = '-';
         }
     }
 
-    public static void actualizarPista(char letra,char[]palabraSecreta, char[]pistaActual){
+    public static void actualizarPista(char letra, char[] palabraSecreta, char[] pistaActual) {
 
         for (int i = 0; i < palabraSecreta.length; i++) {
-            if (Character.toLowerCase(palabraSecreta[i])==Character.toLowerCase(letra)){
-                pistaActual[i]=palabraSecreta[i];
+            if (Character.toLowerCase(palabraSecreta[i]) == Character.toLowerCase(letra)) {
+                pistaActual[i] = palabraSecreta[i];
             }
         }
     }
 
-    public static boolean ganadorPartida (char[]pista, char[]palabraSecreta,char letra){
+    public static boolean ganadorPartida(char[] pista, char[] palabraSecreta, char letra) {
         boolean gana = false;
 
         for (char elemento : palabraSecreta) {
@@ -31,37 +31,38 @@ public class Ejercicio1 {
         }
         return gana;
     }
+
     static void main() {
         String jugadorA;
         char jugadorB;
-        int contador=7;
+        int contador = 7;
         boolean ganador = false;
-        char[]palabraSecreta, pista;
+        char[] palabraSecreta, pista;
 
         System.out.println("\u001B[31m\u001B[1mJugador A.\u001B[0m Escriba una palabra: ");
-        jugadorA=sc.nextLine();
+        jugadorA = sc.nextLine();
 
-        palabraSecreta=jugadorA.toCharArray();
-        pista=new char[palabraSecreta.length];
-        inicioPista(palabraSecreta,pista);
+        palabraSecreta = jugadorA.toCharArray();
+        pista = new char[palabraSecreta.length];
+        inicioPista(palabraSecreta, pista);
 
-        while(!Arrays.equals(pista,palabraSecreta)&&contador>0){
-            System.out.println(String.valueOf(pista) +" Intentos: "+contador);
+        while (!Arrays.equals(pista, palabraSecreta) && contador > 0) {
+            System.out.println(String.valueOf(pista) + " Intentos: " + contador);
             System.out.print("\u001B[34m\u001B[1mJugador B.\u001B[0m Letra: ");
-            jugadorB=sc.next().charAt(0);
+            jugadorB = sc.next().charAt(0);
 
-            actualizarPista(jugadorB,palabraSecreta,pista);
+            actualizarPista(jugadorB, palabraSecreta, pista);
 
-            ganador=ganadorPartida(pista,palabraSecreta,jugadorB);
+            ganador = ganadorPartida(pista, palabraSecreta, jugadorB);
 
-            if (!ganador){
+            if (!ganador) {
                 contador--;
             }
 
         }
-        if (contador==0){
+        if (contador == 0) {
             System.out.println("Gana el jugador A");
-        }else {
+        } else {
             System.out.println("Gana el jugador B");
         }
     }
--- src/main/java/practica6/Ejercicio2.java
+++ src/main/java/practica6/Ejercicio2.java
@@ -3,25 +3,25 @@ package practica6;
 import java.util.Scanner;
 
 public class Ejercicio2 {
-    public static Scanner sc=new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
 
-    public static String primeraMayuscula(String cadena){
-        String primeraMay="";
+    public static String primeraMayuscula(String cadena) {
+        String primeraMay = "";
 
-        primeraMay+=Character.toUpperCase(cadena.charAt(0));
-        primeraMay+=cadena.substring(1);
+        primeraMay += Character.toUpperCase(cadena.charAt(0));
+        primeraMay += cadena.substring(1);
         return primeraMay;
     }
 
-    public static String estiloCamel(String cadena){
-       String minus=cadena.toLowerCase(), estiloCamel = "";
-       String[] frase=minus.split(" ");
+    public static String estiloCamel(String cadena) {
+        String minus = cadena.toLowerCase(), estiloCamel = "";
+        String[] frase = minus.split(" ");
 
         for (int i = 0; i < frase.length; i++) {
-            if (i>0){
-                estiloCamel+=primeraMayuscula(frase[i]);
+            if (i > 0) {
+                estiloCamel += primeraMayuscula(frase[i]);
             } else {
-                estiloCamel+=frase[i];
+                estiloCamel += frase[i];
             }
         }
         return estiloCamel;
@@ -31,9 +31,9 @@ public class Ejercicio2 {
         String frase, estiloCamel;
 
         System.out.print("Escriba una frase: ");
-        frase= sc.nextLine();
+        frase = sc.nextLine();
 
-        estiloCamel=estiloCamel(frase);
-        System.out.println("Variable estilo Camel: "+estiloCamel);
+        estiloCamel = estiloCamel(frase);
+        System.out.println("Variable estilo Camel: " + estiloCamel);
     }
 }
