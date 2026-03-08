58088c704dea6c5a24ee9f8482a1a48528df2149
mikufrost1982-dotcom <mikufrost1982@gmail.com>
2026-03-08 16:45:33 +0100

    hecho falta comentar

---
+++ src/main/java/practica6/Ejercicio2.java
@@ -0,0 +1,39 @@
+package practica6;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static Scanner sc=new Scanner(System.in);
+
+    public static String primeraMayuscula(String cadena){
+        String primeraMay="";
+
+        primeraMay+=Character.toUpperCase(cadena.charAt(0));
+        primeraMay+=cadena.substring(1);
+        return primeraMay;
+    }
+
+    public static String estiloCamel(String cadena){
+       String minus=cadena.toLowerCase(), estiloCamel = "";
+       String[] frase=minus.split(" ");
+
+        for (int i = 0; i < frase.length; i++) {
+            if (i>0){
+                estiloCamel+=primeraMayuscula(frase[i]);
+            } else {
+                estiloCamel+=frase[i];
+            }
+        }
+        return estiloCamel;
+    }
+
+    static void main() {
+        String frase, estiloCamel;
+
+        System.out.print("Escriba una frase: ");
+        frase= sc.nextLine();
+
+        estiloCamel=estiloCamel(frase);
+        System.out.println("Variable estilo Camel: "+estiloCamel);
+    }
+}
