f0dfe6dfda736806f8986242a59a234ec6605898
mikufrost1982-dotcom <mikufrost1982@gmail.com>
2026-03-10 22:04:43 +0100

    Hecho falta comentarlo

--- src/main/java/practica6/Ejercicio3.java
+++ src/main/java/practica6/Ejercicio3.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio3 {
-    public static Scanner sc=new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
 
-    public static void menu(){
+    public static void menu() {
         System.out.println("1. Nuevo contacto.");
         System.out.println("2. Buscar contacto.");
         System.out.println("3. Ver todo.");
@@ -14,100 +14,101 @@ public class Ejercicio3 {
         System.out.println("5. Salir.");
     }
 
-    public static String[]nuevoContacto(String[]agenda, String nombre, String telefono){
-        agenda=Arrays.copyOf(agenda,agenda.length+1);
-        agenda[agenda.length-1]=nombre+" ("+telefono+")";
+    public static String[] nuevoContacto(String[] agenda, String nombre, String telefono) {
+        agenda = Arrays.copyOf(agenda, agenda.length + 1);
+        agenda[agenda.length - 1] = nombre + " (" + telefono + ")";
 
         return agenda;
     }
 
-    public static void buscarContacto(String[] agenda, String nombre){
-        int i=0;
-        String minusNombre=nombre.toLowerCase();
+    public static void buscarContacto(String[] agenda, String nombre) {
+        int i = 0;
+        String minusNombre = nombre.toLowerCase();
 
-        while (i< agenda.length&&!agenda[i].toLowerCase().startsWith(minusNombre)){
+        while (i < agenda.length && !agenda[i].toLowerCase().startsWith(minusNombre)) {
             i++;
         }
 
-        if (i<agenda.length){
-            System.out.println("Nombre: "+agenda[i]+'\n');
-        }else {
+        if (i < agenda.length) {
+            System.out.println("Nombre: " + agenda[i] + '\n');
+        } else {
             System.out.println("El contacto de la persona no existe\n");
         }
     }
 
-    public static void mostrarTodo(String[] agenda){
-        for (String elemento:agenda) {
-            System.out.println("Nombre: "+elemento);
+    public static void mostrarTodo(String[] agenda) {
+        for (String elemento : agenda) {
+            System.out.println("Nombre: " + elemento);
         }
         System.out.println();
     }
 
-    public static String[] borrarContacto(String[] agenda, String nombre){
-        int i=0;
-        String minusNombre=nombre.toLowerCase();
+    public static String[] borrarContacto(String[] agenda, String nombre) {
+        int i = 0;
+        String minusNombre = nombre.toLowerCase();
 
-        while (i< agenda.length&&!agenda[i].toLowerCase().startsWith(minusNombre)){
+        while (i < agenda.length && !agenda[i].toLowerCase().startsWith(minusNombre)) {
             i++;
         }
 
-        if (i< agenda.length) {
+        if (i < agenda.length) {
             agenda[i] = agenda[agenda.length - 1];
             agenda = Arrays.copyOf(agenda, agenda.length - 1);
         }
 
         return agenda;
     }
+
     static void main() {
-        String[]contacto=new String[0];
+        String[] contacto = new String[0];
         String nombrePersona, telefonoContacto;
         int opcion, longitudContacto;
 
         do {
             menu();
             System.out.print("Elige una opción: ");
-            opcion= sc.nextInt();
+            opcion = sc.nextInt();
 
-            switch (opcion){
-                case 1->{
+            switch (opcion) {
+                case 1 -> {
                     sc.nextLine();
                     System.out.print("Nombre: ");
-                    nombrePersona= sc.nextLine();
+                    nombrePersona = sc.nextLine();
 
                     System.out.print("Teléfono: ");
-                    telefonoContacto= sc.nextLine();
+                    telefonoContacto = sc.nextLine();
 
-                    contacto=nuevoContacto(contacto,nombrePersona,telefonoContacto);
+                    contacto = nuevoContacto(contacto, nombrePersona, telefonoContacto);
                     System.out.println();
                 }
 
-                case 2 ->{
+                case 2 -> {
                     sc.nextLine();
                     System.out.print("Nombre a buscar: ");
-                    nombrePersona= sc.nextLine();
+                    nombrePersona = sc.nextLine();
 
-                    buscarContacto(contacto,nombrePersona);
+                    buscarContacto(contacto, nombrePersona);
                 }
 
-                case 3 ->{
+                case 3 -> {
                     mostrarTodo(contacto);
                 }
 
-                case 4->{
+                case 4 -> {
                     sc.nextLine();
                     System.out.print("Introduce el nombre del contacto: ");
-                    nombrePersona= sc.nextLine();
+                    nombrePersona = sc.nextLine();
 
-                    longitudContacto=contacto.length;
-                    contacto=borrarContacto(contacto,nombrePersona);
+                    longitudContacto = contacto.length;
+                    contacto = borrarContacto(contacto, nombrePersona);
 
-                    if (longitudContacto == contacto.length){
+                    if (longitudContacto == contacto.length) {
                         System.out.println("La persona que quieres borrar no está en la agenda\n");
-                    }else {
+                    } else {
                         System.out.println("Contacto eliminado\n");
                     }
                 }
             }
-        }while(opcion!=5);
+        } while (opcion != 5);
     }
 }
