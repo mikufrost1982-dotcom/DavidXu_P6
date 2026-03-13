c6a3e92b755325ddd7f3775edd355346a69ccc70
mikufrost1982-dotcom <mikufrost1982@gmail.com>
2026-03-13 14:21:04 +0100

    Es lo mismo con matrices

---
+++ src/main/java/practica6/Ejercicio4v3StringMatrices.java
@@ -0,0 +1,48 @@
+package practica6;
+
+import java.util.Scanner;
+
+public class Ejercicio4v3StringMatrices {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static String codificar(String frase, String[][] conjuntos) {
+        int indice;
+        String transformaTexto = "", letraMinus = frase.toLowerCase();
+        boolean mayusculas;
+
+        for (int i = 0; i < frase.length(); i++) {
+            indice = conjuntos[0][0].indexOf(letraMinus.charAt(i));
+
+            mayusculas = Character.isUpperCase(frase.charAt(i));
+            if (indice != -1 && mayusculas) {
+                transformaTexto += conjuntos[1][0].toUpperCase().charAt(indice);
+            } else if (indice != -1) {
+                transformaTexto += conjuntos[1][0].charAt(indice);
+            } else {
+                transformaTexto += frase.charAt(i);
+            }
+        }
+        return transformaTexto;
+    }
+
+    static void main() {
+        String[][] conjuntos = {{"eikmpqrstuv"},{"pviumterkqs"}};
+        String frase, codificado, codificarOtra;
+
+        System.out.print("Introduzca un texto a codificar: ");
+        frase = sc.nextLine();
+        do {
+            codificado = codificar(frase, conjuntos);
+
+            System.out.println(codificado);
+
+            System.out.print("¿Quieres codificar otro texto?: ");
+            codificarOtra = sc.nextLine();
+
+            if (codificarOtra.equalsIgnoreCase("si")) {
+                System.out.print("Introduzca un texto a codificar: ");
+                frase = sc.nextLine();
+            }
+        } while (!codificarOtra.equalsIgnoreCase("no"));
+    }
+}
