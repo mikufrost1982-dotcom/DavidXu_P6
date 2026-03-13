79a2b4c4d8616d2c2d95ee668f0e6bb1483556f9
mikufrost1982-dotcom <mikufrost1982@gmail.com>
2026-03-13 16:32:41 +0100

    hecho

--- src/main/java/practica6/Ejercicio1.java
+++ src/main/java/practica6/Ejercicio1.java
@@ -3,23 +3,56 @@ package practica6;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/***
+ * Este programa es el juego que todas las personas ha jugado y es el ahorcado. El juego consiste en adivinar
+ * la palabra secreta que empieza por '-', tienes que introducir una letra si aciertas se sigue, si no tienes 7
+ * intentos que se sustituye por la persona en palitos
+ * @author David Xu
+ * @version v1 13/03/2026
+ */
 public class Ejercicio1 {
+    //Función Scanner que es global o mejor dicho que se puede usar en cualquier lado
     public static Scanner sc = new Scanner(System.in);
 
-    public static void inicioJuego(char[] palabraSecreta, char[] pista) {
+    //Estos son las secuencias de Escape ANSI que se hacen en codepoint de hexadecimal generado por Internet
+    public static final String ROJO_NEGRITA="\u001B[31m\u001B[1m";
+    public static final String AZUL_NEGRITA="\u001B[34m\u001B[1m";
+    public static final String RESET="\u001B[0m";
+
+    /***
+     * El juego ahorcado la palabra secreta empieza en guiones, es el inicio del juego cuando empiezas a adivinar las letras
+     * @param palabraSecreta Recibe la palabra secreta en un array de tipo char
+     * @param inicio Recibe una variable vacía rellena los '-' por la longitud de la palabra secreta
+     */
+    public static void inicioJuego(char[] palabraSecreta, char[] inicio) {
         for (int i = 0; i < palabraSecreta.length; i++) {
-            pista[i] = '-';
+            inicio[i] = '-';
         }
     }
 
-    public static void ahorcado(char letra, char[] palabraSecreta, char[] pistaActual) {
+    /***
+     * Este juego no es en pedir la pista, si no es en adivinar la letra de la palabra secreta y que te lo muestre la letra
+     * y los guiones las dejo y no lo voy a tocar
+     * @param letra Recibe una letra que ha sido introducido por el jugador que está adivinando
+     * @param palabraSecreta Recibe la palabra secreta en un array de tipo char
+     * @param letraAcertada Recibe los '-' de tipo char[] y guarda las letras que esté acertando o dejas en '-'
+     */
+    public static void ahorcado(char letra, char[] palabraSecreta, char[] letraAcertada) {
+        char letraMinus = Character.toLowerCase(letra);
         for (int i = 0; i < palabraSecreta.length; i++) {
-            if (Character.toLowerCase(palabraSecreta[i]) == Character.toLowerCase(letra)) {
-                pistaActual[i] = palabraSecreta[i];
+            if (Character.toLowerCase(palabraSecreta[i]) == letraMinus) {
+                letraAcertada[i] = palabraSecreta[i];
             }
         }
     }
 
+    /***
+     * Este comprueba si el jugador que está adivinando y adivina la palabra secreta gana
+     * @param palabraSecreta Recibe la palabra secreta de tipo char[]
+     * @param letra Recibe la letra que introduce el jugador que esté adivinando
+     * @return Devuelve true si el elemento o la palabraSecreta de las letras es igual a la letra
+     * que introduce el jugador que esté adivinando, si no false
+     */
     public static boolean ganadorPartida(char[] palabraSecreta, char letra) {
         boolean gana = false;
 
@@ -32,33 +65,47 @@ public class Ejercicio1 {
     }
 
     static void main() {
+        //Variables
         String jugadorA;
         char jugadorB;
         int contador = 7;
         boolean ganador;
-        char[] palabraSecreta, pista;
+        char[] palabraSecreta, letraAcertada;
 
-        System.out.println("\u001B[31m\u001B[1mJugador A.\u001B[0m Escriba una palabra: ");
+        //El jugador A es el que introduce la palabra secreta y mostrara la longitud que empezara los guiones
+        System.out.println(ROJO_NEGRITA+"Jugador A."+RESET+" Escriba una palabra: ");
         jugadorA = sc.nextLine();
 
+        //La palabra secreta lo convierto en un array para recorrer en un bucle
         palabraSecreta = jugadorA.toCharArray();
-        pista = new char[palabraSecreta.length];
-        inicioJuego(palabraSecreta, pista);
+        //Creo para guardar las letras acertadas o el inicio del juego en '-' o las dejo
+        letraAcertada = new char[palabraSecreta.length];
+        //Llamo a la función para empezar el juego con '-'
+        inicioJuego(palabraSecreta, letraAcertada);
 
-        while (!Arrays.equals(pista, palabraSecreta) && contador > 0) {
-            System.out.println(String.valueOf(pista) + " Intentos: " + contador);
-            System.out.print("\u001B[34m\u001B[1mJugador B.\u001B[0m Letra: ");
+        //Aquí estoy diciendo que si el contador es mayor que 0 y si el variable array que está guardando la letra
+        //no es igual a la palabra secreta se sigue
+        while (!Arrays.equals(letraAcertada, palabraSecreta) && contador > 0) {
+            //Uso String.valueOf para que me pase el array en un String y muestra los intentos y las letras acertadas
+            System.out.println(String.valueOf(letraAcertada) + " Intentos: " + contador);
+            //El jugador B introduce una letra para adivinar la palabra secreta
+            System.out.print(AZUL_NEGRITA+"Jugador B."+RESET+" Letra: ");
             jugadorB = sc.next().charAt(0);
 
-            ahorcado(jugadorB, palabraSecreta, pista);
+            //Llamo a la función para que me muestre la letra acertada
+            ahorcado(jugadorB, palabraSecreta, letraAcertada);
 
+            //Guardo la función en un variable
             ganador = ganadorPartida(palabraSecreta, jugadorB);
 
+            //Si la letra no coincide hago un -1 los intentos
             if (!ganador) {
                 contador--;
             }
-
         }
+
+        //Para saber quien gana utilizamos el contador porque son los intentos o las oportunidades para adivinarlo
+        //la palabra secreta
         if (contador == 0) {
             System.out.println("Gana el jugador A");
         } else {
--- src/main/java/practica6/Ejercicio2.java
+++ src/main/java/practica6/Ejercicio2.java
@@ -2,9 +2,19 @@ package practica6;
 
 import java.util.Scanner;
 
+/***
+ * Estilo camel es una regla de variables como ejemplo meGustaMerendarGalletas
+ * @author David Xu
+ * @version v1 13/03/2026
+ */
 public class Ejercicio2 {
     public static Scanner sc = new Scanner(System.in);
 
+    /***
+     * La segunda palabra me pone la primera letra en mayúsculas y el resto en minúsculas
+     * @param cadena Recibe la palabra de la función split que le invocó estiloCamel()
+     * @return Devuelve la primera letra en mayúsculas y el resto en minúsculas
+     */
     public static String primeraMayuscula(String cadena) {
         String primeraMay = "";
 
@@ -13,6 +23,13 @@ public class Ejercicio2 {
         return primeraMay;
     }
 
+    /***
+     * Recorre toda la frase hago un split para convertir en un String[] donde los espacios me conviertas en una
+     * coma y si la variable i es mayor que cero llamo a la función primeraMayúsculas() y si no se queda normal, en este caso
+     * es la primera que no se queda en mayúsculas
+     * @param cadena Recibe la frase que introduce el usuario
+     * @return Devuelve un variable en estilo camel
+     */
     public static String estiloCamel(String cadena) {
         String minus = cadena.toLowerCase(), estiloCamel = "";
         String[] frase = minus.split(" ");
@@ -28,11 +45,14 @@ public class Ejercicio2 {
     }
 
     static void main() {
+        //Variables
         String frase, estiloCamel;
 
+        //Pido al usuario que introduzca una frase para convertir la frase en estilo camel
         System.out.print("Escriba una frase: ");
         frase = sc.nextLine();
 
+        //Llamó a la función y que convierta el estilo camel y lo muestro
         estiloCamel = estiloCamel(frase);
         System.out.println("Variable estilo Camel: " + estiloCamel);
     }
